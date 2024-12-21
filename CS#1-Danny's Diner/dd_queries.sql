1d46249e312876e9a469a63fa25690f5ce7828bc
medprod <medhaa.prodduturi@gmail.com>
2024-12-21 11:58:04 -0600

    dd queries

--- CS#1-Danny's Diner/dd_queries.sql
+++ CS#1-Danny's Diner/dd_queries.sql
@@ -73,7 +73,6 @@ ORDER BY CustomerID;
 
 
 --7.Which item was purchased just before the customer became a member?
---here I am assuming customerID A bought food then became member on 2021-01-07.
 WITH BeforeBought AS (
     SELECT s.customer_id AS CustomerID,
     m.product_name AS ProductName,
@@ -82,7 +81,7 @@ WITH BeforeBought AS (
     FROM Sales s
     JOIN Menu m ON s.product_id = m.product_id
     JOIN Members mem ON s.customer_id = mem.customer_id
-    WHERE s.order_date <= mem.join_date
+    WHERE s.order_date < mem.join_date
 )
 
 SELECT CustomerID, ProductName
@@ -94,3 +93,56 @@ WHERE RowRank=1
 ORDER BY CustomerID;
 
 --8.What is the total items and amount spent for each member before they became a member?
+WITH TotalSummary AS(
+    SELECT s.customer_id AS customerID, 
+    COUNT(s.product_id) AS TotalItems,
+    SUM(m.price) AS AmountSpent,
+    s.order_date AS DateOrdered,
+    mem.join_date AS JoinedDate
+    FROM Sales s 
+    JOIN Menu m ON s.product_id = m.product_id
+    JOIN Members mem ON s.customer_id = mem.customer_id
+    GROUP BY s.customer_id,s.order_date,mem.join_date
+    HAVING s.order_date < mem.join_date
+)
+
+SELECT customerID, 
+SUM(TotalItems) AS 'Total Items', 
+SUM(AmountSpent) AS 'Amount Spent'
+FROM TotalSummary
+GROUP BY customerID;
+
+--9.If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?
+SELECT s.customer_id, 
+SUM(CASE WHEN m.product_id = 1 THEN m.price * 20 ELSE m.price * 10 END) AS 'Customer Points'
+FROM Sales s
+JOIN Menu m 
+ON s.product_id = m.product_id
+GROUP BY s.customer_id;
+
+--10.In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi - how many points do customer A and B have at the end of January?
+WITH TotalPoints AS(
+    SELECT s.customer_id AS CustomerID,s.order_date AS OrderDate,
+    SUM(CASE 
+        WHEN s.order_date BETWEEN mem.join_date AND DATEADD(day, 6, mem.join_date) THEN m.price * 20
+        WHEN s.order_date NOT BETWEEN mem.join_date AND DATEADD(day, 6, mem.join_date) AND m.product_id = 1 THEN m.price * 20
+        ELSE m.price * 10 END) AS CustomerPoints
+    FROM Sales s
+    JOIN Menu m 
+    ON s.product_id = m.product_id 
+    JOIN Members mem
+    ON s.customer_id = mem.customer_id
+    GROUP BY s.customer_id,s.order_date
+    HAVING MONTH(s.order_date) = 1)
+
+SELECT CustomerID, SUM(CustomerPoints)
+FROM TotalPoints
+GROUP BY CustomerID;
+
+
+
+
+
+
+
+
