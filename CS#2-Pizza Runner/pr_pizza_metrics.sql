bbc18ad45d73b148f732593d32325f96491ff5d8
medprod <medhaa.prodduturi@gmail.com>
2025-01-04 19:33:29 -0600

    pizza runner

--- CS#2-Pizza Runner/pr_pizza_metrics.sql
+++ CS#2-Pizza Runner/pr_pizza_metrics.sql
@@ -33,3 +33,15 @@ COUNT(c.order_id) AS 'Number of Pizzas Ordered'
 FROM customer_orders c
 JOIN pizza_names p ON c.pizza_id = p.pizza_id
 GROUP BY c.customer_id, p.pizza_name
+
+--What was the maximum number of pizzas delivered in a single order?
+SELECT TOP 1 COUNT(c.pizza_id) AS 'Max Number of Delivered Pizzas', c.order_id, r.pickup_time 
+FROM customer_orders c
+JOIN runner_orders r ON c.order_id = r.order_id
+WHERE r.pickup_time != 'null'
+GROUP BY c.order_id, r.pickup_time 
+ORDER BY COUNT(c.pizza_id) DESC
+
+--For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
+
+
