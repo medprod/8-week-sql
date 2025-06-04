307242f91ea2ef4cc85583b4e89808504ed2fe1a
medprod <medhaa.prodduturi@gmail.com>
2025-06-03 21:33:22 -0500

    case 3

--- CS#3-Foodie-Fi /ff_partB.sql
+++ CS#3-Foodie-Fi /ff_partB.sql
@@ -1,4 +1,4 @@
-SELECT * FROM foodie_fi.plans;
+SELECT * FROM foodie_fi.plans LIMIT 10;
 
 SELECT * FROM foodie_fi.subscriptions;
 
@@ -8,5 +8,28 @@ SELECT * FROM foodie_fi.subscriptions;
 SELECT COUNT(DISTINCT(customer_id)) AS total_customers
 FROM foodie_fi.subscriptions;
 
---2. What is the monthly distribution of trial plan start_date values for our dataset - 
+--2. What is the monthly distribution of trial plan start_date values for our dataset 
 -- use the start of the month as the group by value
+SELECT COUNT(p.plan_id) AS total_subscriptions, date_trunc('month',s.start_date) AS month_start
+FROM foodie_fi.plans p
+JOIN foodie_fi.subscriptions s ON p.plan_id = s.plan_id
+GROUP BY date_trunc('month',s.start_date)
+ORDER BY month_start;
+
+--3. What plan start_date values occur after the year 2020 for our dataset? 
+--Show the breakdown by count of events for each plan_name
+SELECT COUNT(p.plan_id), p.plan_name, date_trunc('month',s.start_date) AS month_start 
+FROM foodie_fi.plans p
+JOIN foodie_fi.subscriptions s ON p.plan_id = s.plan_id
+WHERE date_part('month',s.start_date) > 2020
+GROUP BY p.plan_name, month_start
+ORDER BY month_start;
+
+
+--4. What is the customer count and percentage of customers who have churned rounded 
+--to 1 decimal place?
+
+SELECT COUNT(*) FILTER (WHERE p.plan_id = 4) AS total_churned,
+ROUND(100.0* COUNT(*) FILTER (WHERE p.plan_id = 4)/COUNT(DISTINCT s.customer_id),1) AS churn_percenatge
+FROM foodie_fi.subscriptions s
+JOIN foodie_fi.plans p ON s.plan_id = p.plan_id;
