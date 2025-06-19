6d2d165de6123c53bc613e3fd349dcea430f8f14
medprod <medhaa.prodduturi@gmail.com>
2025-06-18 20:26:46 -0500

    case 2

--- CS#2-Pizza Runner/pr_creation.sql
+++ CS#2-Pizza Runner/pr_creation.sql
@@ -113,3 +113,33 @@ VALUES
   (10, 'Salami'),
   (11, 'Tomatoes'),
   (12, 'Tomato Sauce');
+
+--EDIT TABLES ACCORDINGLY
+
+--removing null values from runner_orders and customer_orders tables
+UPDATE pizza_runner.runner_orders
+SET 
+  pickup_time = CASE WHEN pickup_time IS NULL OR pickup_time = 'null' THEN '' ELSE pickup_time END,
+  distance = CASE WHEN distance IS NULL OR distance = 'null' THEN '' ELSE distance END,
+  duration = CASE WHEN duration IS NULL OR duration = 'null' THEN '' ELSE duration END,
+  cancellation = CASE WHEN cancellation IS NULL OR cancellation = 'null' THEN '' ELSE cancellation END
+WHERE 
+  pickup_time IS NULL OR pickup_time = 'null' OR
+  distance IS NULL OR distance = 'null' OR
+  duration IS NULL OR duration = 'null' OR
+  cancellation IS NULL OR cancellation = 'null';
+
+UPDATE pizza_runner.customer_orders
+SET 
+  exclusions = CASE WHEN exclusions IS NULL OR exclusions = 'null' THEN '' ELSE exclusions END,
+  extras = CASE WHEN extras IS NULL OR extras = 'null' THEN '' ELSE extras END
+WHERE 
+  extras IS NULL OR extras = 'null' OR
+  exclusions IS NULL OR exclusions = 'null';
+
+
+--changing the data type of pickup_time from varchar to timestamp
+ALTER TABLE pizza_runner.runner_orders
+ALTER COLUMN pickup_time TYPE TIMESTAMP
+USING to_timestamp(pickup_time, 'YYYY-MM-DD HH24:MI:SS')
+
---
+++ CS#2-Pizza Runner/pr_partB.sql
@@ -0,0 +1,8 @@
+SELECT * FROM pizza_runner.runners;
+SELECT * FROM pizza_runner.customer_orders;
+SELECT * FROM pizza_runner.runner_orders;
+SELECT * FROM pizza_runner.pizza_names;
+SELECT * FROM pizza_runner.pizza_recipes;
+SELECT * FROM pizza_runner.pizza_toppings;
+
+--Runner and Customer Experience
