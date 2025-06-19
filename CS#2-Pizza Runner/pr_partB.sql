a3f479a97489c00283d9bbc90cf4ef4ff8ea157d
medprod <medhaa.prodduturi@gmail.com>
2025-06-18 21:21:16 -0500

    case 2

--- CS#2-Pizza Runner/pr_partB.sql
+++ CS#2-Pizza Runner/pr_partB.sql
@@ -6,3 +6,21 @@ SELECT * FROM pizza_runner.pizza_recipes;
 SELECT * FROM pizza_runner.pizza_toppings;
 
 --Runner and Customer Experience
+
+--1.How many runners signed up for each 1 week period? (i.e. week starts 2021-01-01)
+WITH week_cte AS 
+	(SELECT runner_id, registration_date,
+	registration_date - ((registration_date - '2021-01-01')%7) AS week_start
+	FROM pizza_runner.runners
+	)
+SELECT COUNT(runner_id), 
+week_start
+FROM week_cte
+GROUP BY week_start
+ORDER BY week_start;
+
+--2.What was the average time in minutes it took for each runner to arrive at the Pizza Runner HQ to pickup the order?
+
+	
+
+
