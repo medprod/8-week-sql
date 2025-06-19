7b44589f7f5d03ff3de343b3527ebc7abe594be3
medprod <medhaa.prodduturi@gmail.com>
2025-06-18 21:52:19 -0500

    case 2

--- CS#2-Pizza Runner/pr_partB.sql
+++ CS#2-Pizza Runner/pr_partB.sql
@@ -20,6 +20,23 @@ GROUP BY week_start
 ORDER BY week_start;
 
 --2.What was the average time in minutes it took for each runner to arrive at the Pizza Runner HQ to pickup the order?
+WITH avg_time AS(
+	SELECT r.order_id, r.runner_id, 
+	date_part('minute', r.pickup_time) as pickup_min,
+	date_part('minute', c.order_time) as order_min,
+	r.pickup_time - c.order_time AS time_diff
+	FROM pizza_runner.runner_orders r
+	JOIN pizza_runner.customer_orders c
+	ON r.order_id = c.order_id
+	WHERE distance != '' AND duration != ''
+	)
+SELECT runner_id, ROUND(AVG(date_part('minute', time_diff))::numeric, 0)
+FROM avg_time
+GROUP BY runner_id
+ORDER BY runner_id;
+
+
+
 
 	
 
