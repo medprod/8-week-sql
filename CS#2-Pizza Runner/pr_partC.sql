400601a00a935475eebb75ae58873fc5c8159b28
medprod <medhaa.prodduturi@gmail.com>
2025-06-23 22:28:52 -0500

    case 2

--- CS#2-Pizza Runner/pr_partC.sql
+++ CS#2-Pizza Runner/pr_partC.sql
@@ -34,7 +34,7 @@ ORDER BY pt.pizza_id, pt.toppings
 --2.What was the most commonly added extra?
 SELECT * FROM pizza_runner.customer_orders;
 
-CREATE TEMP TABLE orders_temp(
+CREATE TEMP TABLE extras_temp(
 	order_id INT,
 	customer_id INT,
 	pizza_id INT,
@@ -42,20 +42,22 @@ CREATE TEMP TABLE orders_temp(
 	extras text,
 	ordertime timestamp
 );
-INSERT INTO orders_temp(order_id, customer_id, pizza_id, exclusions, extras, ordertime)
-SELECT order_id, customer_id, pizza_id, unnest(string_to_array(exclusions,',')),
-unnest(string_to_array(extras,',')), order_time
-FROM pizza_runner.customer_orders
+INSERT INTO extras_temp (order_id, customer_id, pizza_id, exclusions, extras, ordertime)
+SELECT 
+  co.order_id,
+  co.customer_id,
+  co.pizza_id,
+  co.exclusions,
+  x.extras,
+  co.order_time
+FROM pizza_runner.customer_orders co
+LEFT JOIN LATERAL unnest(string_to_array(co.extras, ',')) AS x(extras) ON TRUE;
 
-ALTER TABLE orders_temp
+ALTER TABLE extras_temp
 ALTER column extras TYPE numeric
 USING extras::numeric;
 
-ALTER TABLE orders_temp
-ALTER column exclusions TYPE numeric
-USING exclusions::numeric;
-
-SELECT * FROM orders_temp;
+SELECT * FROM extras_temp;
 SELECT * FROM pizza_runner.pizza_toppings;
 
 --query for 2
@@ -64,3 +66,46 @@ FROM orders_temp o
 JOIN pizza_runner.pizza_toppings p ON o.extras = p.topping_id
 GROUP BY o.extras, p.topping_name
 ORDER BY extras_count DESC
+
+--3.What was the most common exclusion?
+
+CREATE TEMP TABLE exclusions_temp(
+	order_id INT,
+	customer_id INT,
+	pizza_id INT,
+	exclusions text, 
+	extras text,
+	ordertime timestamp
+);
+INSERT INTO exclusions_temp (order_id, customer_id, pizza_id, exclusions, extras, ordertime)
+SELECT 
+  co.order_id,
+  co.customer_id,
+  co.pizza_id,
+  e.exclusions,
+  co.extras,
+  co.order_time
+FROM pizza_runner.customer_orders co
+LEFT JOIN LATERAL unnest(string_to_array(co.exclusions, ',')) AS e(exclusions) ON TRUE;
+
+ALTER TABLE exclusions_temp
+ALTER column exclusions TYPE numeric
+USING exclusions::numeric;
+
+SELECT * FROM exclusions_temp;
+
+SELECT p.topping_name, COUNT(*) AS exclusions_count
+FROM exclusions_temp e
+JOIN pizza_runner.pizza_toppings p ON e.exclusions = p.topping_id
+GROUP BY e.exclusions, p.topping_name
+ORDER BY exclusions_count DESC 
+
+--4. Generate an order item for each record in the customers_orders table
+SELECT * FROM extras_temp;
+SELECT * FROM exclusions_temp;
+
+
+
+
+
+
